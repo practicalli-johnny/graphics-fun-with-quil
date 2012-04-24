1f9656e2ffaff3f24a570a79aa03f1c834912bcc
John Stevenson <john@jr0cket.co.uk>
2012-04-24 16:35:40 +0100

    Added Quil dependencies to make use of processing library

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://clojuremadesimple.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [quil "1.3.0"]])
 

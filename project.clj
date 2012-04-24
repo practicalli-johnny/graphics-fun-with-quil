5d4eff49299d2f5b7add8c4e5dd2ff193e4524a2
John Stevenson <john@jr0cket.co.uk>
2012-04-24 16:33:56 +0100

    Initial project creation

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject graphics-fun "0.1.0-SNAPSHOT"
+  :description "Some fun with graphics for the April 2012 dojo"
+  :url "http://clojuremadesimple.com/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]])
+
---
+++ test/graphics_fun/core_test.clj
@@ -0,0 +1,7 @@
+(ns graphics-fun.core-test
+  (:use clojure.test
+        graphics-fun.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
